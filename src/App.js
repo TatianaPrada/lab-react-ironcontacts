5432e66cbdec2e40258b650364149912d3d6e862
TatianaPrada <tatiana.1227@hotmail.com>
2021-12-01 15:29:00 +0000

    interaction 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
+const contactsArr = contacts.slice(0, 5)
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+
+  <h2>Iron Contacts</h2>
+    <table>
+      <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      </thead>
+      {contactsArr.map((contact, index) => {
+        return (
+        <tbody>
+          <tr>
+          <td><img src={contact.pictureUrl} alt="artist" /></td>       
+          <td>{contact.name}</td>       
+          <td>{contact.popularity.toFixed(2)}</td>       
+          </tr>
+        </tbody>
+        )
+    })}
+
+  </table>
+  </div>
 }
 
 export default App;
